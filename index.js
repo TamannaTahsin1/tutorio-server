a494dca49de07172f399c01567105c0a4b7a2008
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-11-26 11:05:07 +0600

    data load in server

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 5000
+/** @format */
+
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('Tutorio Server is Running')
-})
-app.listen(port, () =>{
-    console.log(`Tutorio Server is Running on port ${port}`)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5hejig.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    //*******ALL COLLECTIONS********/
+    const classesCollection = client.db("tutorioDb").collection("classes");
+
+    //*****CLASSES API*****/
+    // get data
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// initial running
+app.get("/", (req, res) => {
+  res.send("Tutorio Server is Running");
+});
+app.listen(port, () => {
+  console.log(`Tutorio Server is Running on port ${port}`);
+});
