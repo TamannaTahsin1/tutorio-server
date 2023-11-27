6122c40fbedff028c077278f3e5d64a722e6cac0
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-11-27 11:56:08 +0600

    create token and implementing jwt

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+/** @format */
+
 const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -38,38 +41,75 @@ async function run() {
     //*****CART API*****/
     // get data
     app.get("/carts", async (req, res) => {
-        const email = req.query.email;
-        const query = {email:email}
-        const result = await cartCollection.find(query).toArray();
-        res.send(result);
-      });
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
     // post data
     app.post("/carts", async (req, res) => {
-        const cartItem = req.body;
+      const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
     // delete data
-    app.delete('/carts/:id', async(req, res) =>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await cartCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     //**********USERS RELATED API************/
+    // get data
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     // post data
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user does not exist
-      const query = {email:user.email}
-      const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message:'user already exists'})
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
       }
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  });
+    });
+
+    // patch data
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // delete data
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ********JWT API************
+    // post data
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365D",
+      });
+      res.send({ token });
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
